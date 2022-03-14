53b530c65ba2048440ca885b0c87c6e4baa1e041
Konstantin <konstantin.petrikevich@gmail.com>
2022-03-14 22:20:22 +0300

    Added User models and configure moodules

---
+++ marketplace/serializers.py
@@ -0,0 +1,13 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 
+            'name', 'surname', 
+            'phone'
+             )
---
+++ marketplace/views.py
@@ -0,0 +1,11 @@
+from unicodedata import category
+from django import views
+from rest_framework import generics, viewsets
+from .models import  User
+from .serializers import UserSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
